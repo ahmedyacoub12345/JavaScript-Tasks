2b4be6d50e4b8925882c6ad4e5b5f87839ed5474
ahmedyacoub12345 <ahmedyacoub718@gmail.com>
2024-06-12 11:21:34 +0300

    TASK4

--- Javascr.js
+++ Javascr.js
@@ -61,45 +61,96 @@ document.getElementById('Info').addEventListener('submit', function(event) {
 
 
 
-function validateInputCustom(inputId, regex, errorMessage) {
-    const input = document.getElementById(inputId);
-    const errorSpan = document.getElementById(inputId + 's');
-    if (!regex.test(input.value)) {
-        errorSpan.style.visibility = 'visible';
-    } else {
-        errorSpan.style.visibility = 'hidden';
+function creation(ob,cardid) {
+    const container = document.getElementById(cardid); 
+    const p = document.createElement('p');
+    for (let key in ob) {
+        const line = document.createElement('p');
+        line.innerHTML = ob[key];
+        p.appendChild(line);
     }
+    container.appendChild(p);
 }
+const Fname = document.getElementById("inp1");
+const Lname = document.getElementById("inp2");
+const Bdate = document.getElementById("inp3");
+const email = document.getElementById("inp4");
+const emailConf = document.getElementById("inp5");
+const pass = document.getElementById("inp6");
+const passConf = document.getElementById("inp7");
+const namePat = /^[a-zA-Z]+/;
+const datePat = /^(0?[1-9]|[12][0-9]|3[01])\/(0?[1-9]|1[0-2])\/\d{4}$/;
+const EmailPat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const passPat = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+const sp1 = document.getElementById("sp1");
+const sp2 = document.getElementById("sp2");
+const sp3 = document.getElementById("sp3");
+const sp4 = document.getElementById("sp4");
+const sp5 = document.getElementById("sp5");
+const sp6 = document.getElementById("sp6");
+const sp7 = document.getElementById("sp7");
 
-function validateEmailMatchCustom() {
-    const email1 = document.getElementById('email').value;
-    const email2 = document.getElementById('email2').value;
-    const errorSpan = document.getElementById('email2s');
-    if (email1 !== email2) {
-        errorSpan.style.visibility = 'visible';
+Fname.addEventListener('input', function () {
+    const inp_V = Fname.value.trim();
+    if (!namePat.test(inp_V)) {
+        sp1.innerHTML = "Numbers and Symbols are not allowed";
+        sp1.style.color = "red";
     } else {
-        errorSpan.style.visibility = 'hidden';
+        sp1.innerHTML = "";
     }
-}
+});
 
-function validatePassMatchCustom() {
-    const pass1 = document.getElementById('pass').value;
-    const pass2 = document.getElementById('pass2').value;
-    const errorSpan = document.getElementById('pass2s');
-    if (pass1 !== pass2) {
-        errorSpan.style.visibility = 'visible';
+Lname.addEventListener('input', function () {
+    const inp_V = Lname.value.trim();
+    if (!namePat.test(inp_V)) {
+        sp2.innerHTML = "Numbers and Symbols are not allowed";
+        sp2.style.color = "red";
     } else {
-        errorSpan.style.visibility = 'hidden';
+        sp2.innerHTML = "";
     }
-}
+});
 
-function validateBirthDate() {
-    const birthDate = document.getElementById('Bdate').value;
-    const errorSpan = document.getElementById('bdates');
-    const regex = /^(\d{2})\/(\d{2})\/(\d{4})$/;
-    if (!regex.test(birthDate)) {
-        errorSpan.style.visibility = 'visible';
+Bdate.addEventListener('input', function () {
+    if (!datePat.test(Bdate.value)) {
+        sp3.innerHTML = "The format should be DD/MM/YY";
+        sp3.style.color = "red";
     } else {
-        errorSpan.style.visibility = 'hidden';
+        sp3.innerHTML = "";
     }
-}
+});
+
+email.addEventListener('input', function () {
+    if (!EmailPat.test(email.value)) {
+        sp4.innerHTML = "The e-mail is not valid";
+        sp4.style.color = "red";
+    } else {
+        sp4.innerHTML = "";
+    }
+});
+
+emailConf.addEventListener('input', function () {
+    if (email.value !== emailConf.value) {
+        sp5.innerHTML = "The e-mail not matching";
+        sp5.style.color = "red";
+    } else {
+        sp5.innerHTML = "";
+    }
+});
+
+pass.addEventListener('input', function () {
+    if (!passPat.test(pass.value)) {
+        sp6.innerHTML = "The passwords not valid";
+        sp6.style.color = "red";
+    } else {
+        sp6.innerHTML = "";
+    }
+});
+
+passConf.addEventListener('input', function () {
+    if (pass.value !== passConf.value) {
+        sp7.innerHTML = "The passwords not matching";
+        sp7.style.color = "red";
+    } else {
+        sp7.innerHTML = "";
+    }
+});
